a91b1145e15fe69c2065eb25df5de060646817a8
Jonathan-MG <jonathanmgomes10@hotmail.com>
2022-09-27 15:32:10 -0300

    Setup inicial da página principal e do carrinho

--- Projeto Loja Online - T3/src/main.py
+++ Projeto Loja Online - T3/src/main.py
@@ -1,11 +1,11 @@
 import time
 import streamlit as st
 
-titulo = "Login"
-st.set_page_config(page_title=titulo,layout="centered",initial_sidebar_state="collapsed",menu_items=None)
-st.title(titulo)
+st.set_page_config(page_title="Snoteam",layout="wide",initial_sidebar_state="collapsed",menu_items=None)
+
 def check_password():
     """Returns `True` if the user had a correct password."""
+    
     def password_entered():
         """Checks whether a password entered by the user is correct."""
         if (
@@ -31,6 +31,7 @@ def check_password():
             if st.button(label="Esqueci a senha"):
                 st.write("Redirecionando...")
         return False
+    
     elif not st.session_state["password_correct"]:
         # Password not correct, show input + error.
         st.text_input(label="Digite seu E-mail:", key="username")
@@ -44,12 +45,55 @@ def check_password():
                 st.write("Redirecionando...")
         st.error("😕 User not known or password incorrect")
         return False
+    
     else:
         # Password correct.
         return True
 
 if check_password():
-    destaques,lancamentos,ofertas,perfil = st.tabs(["Destaques","Lançamentos","Ofertas","Perfil"])
+    loja,carrinho = st.tabs(["Loja","Carrinho"])
+    
+    with loja:
+        st.subheader("Destaques")
+        col1,col2,col3 = st.columns(3)
+        with col1:
+            st.write("1")
+        
+        with col2:
+            st.write("2")
+        
+        with col3:
+            st.write("3")
+        
+        st.subheader("Ofertas")
+        col4,col5,col6 = st.columns(3)
+        with col4:
+            st.write("1")
+        
+        with col5:
+            st.write("2")
+        
+        with col6:
+            st.write("3")
+        
+        st.subheader("Pre-Order")
+        col7,col8,col9 = st.columns(3)
+        with col7:
+            st.write("1")
+        
+        with col8:
+            st.write("2")
+        
+        with col9:
+            st.write("3")
+    
+    with carrinho:
+        col1,col2 = st.columns([3,1])
+        with col1:
+            st.write("Itens")
+        
+        with col2:
+            st.write("Resumo da Compra:")
 
 hide_menu_style = """
         <style>
@@ -57,55 +101,3 @@ hide_menu_style = """
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
-
-# with st.sidebar:
-#     with st.echo():
-#         st.write("This code will be printed to the sidebar.")
-
-#     with st.spinner("Loading..."):
-#         time.sleep(5)
-#     st.success("Done!")
-
-# # Using object notation
-# add_selectbox = st.sidebar.selectbox(
-#     "How would you like to be contacted?",
-#     ("Email", "Home phone", "Mobile phone")
-# )
-
-# # Using "with" notation
-# with st.sidebar:
-#     add_radio = st.radio(
-#         "Choose a shipping method",
-#         ("Standard (5-15 days)", "Express (2-5 days)")
-#     )
-
-# # Store the initial value of widgets in session state
-# if "visibility" not in st.session_state:
-#     st.session_state.visibility = "visible"
-#     st.session_state.disabled = False
-
-# col1,col2 = st.columns(2)
-
-# with col1:
-#     st.checkbox("Disable text input widget", key="disabled")
-#     st.radio(
-#         "Set text input label visibility 👉",
-#         key="visibility",
-#         options=["visible", "hidden", "collapsed"],
-#     )
-#     st.text_input(
-#         "Placeholder for the other text input widget",
-#         "This is a placeholder",
-#         key="placeholder",
-#     )
-
-# with col2:
-#     text_input = st.text_input(
-#         "Enter some text 👇",
-#         label_visibility=st.session_state.visibility,
-#         disabled=st.session_state.disabled,
-#         placeholder=st.session_state.placeholder,
-#     )
-
-#     if text_input:
-#         st.write("You entered: ", text_input)        
